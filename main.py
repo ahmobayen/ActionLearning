58be20198e718873c1bd75a92c1e6c545bf9570b
amir <A.H.Mobayen@Gmail.com>
2023-07-27 19:55:06 +0200

    move readme to main

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import streamlit as st
+def readme_main():
+    st.markdown(get_readme_contents(), unsafe_allow_html=True)
 
 
-st.title("Stock Market Analysis and Price Predictor Web App")
+def get_readme_contents():
+    with open("README.md", "r") as readme_file:
+        contents = readme_file.read()
+    return contents
+
+
+readme_main()
